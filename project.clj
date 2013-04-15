84d9b04ecaa99eafd375de357189ebd3c437a0eb
Ragnar Dahlén <r.dahlen@gmail.com>
2013-04-15 17:37:55 +0100

    Simplifying project.clj slightly

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject defrecordissue "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:aot1 {:aot [defrecordissue.aot1]}
-             :aot2 {:aot [defrecordissue.aot2]}}
-  :aliases {"aot1" ["do" "clean," "with-profile" "aot1" "compile"]
-            "aot2" ["do" "clean," "with-profile" "aot2" "compile"]})
+  :aliases {"aot1" ["do" "clean," "compile" "defrecordissue.aot1"]
+            "aot2" ["do" "clean," "compile" "defrecordissue.aot2"]})
